b48f79ce71b73d086cc4af1d518b1ac44b58f5f5
CharlBest <charlbest@yahoo.com>
2017-11-28 15:38:41 +0200

    udpate apiUrlEndpoint for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrlEndpoint: 'https://things-cx-api.herokuapp.com/',
+  apiUrlEndpoint: 'https://things-cx-api.herokuapp.com',
   googleCustomSearchUrlEndpoint: 'https://www.googleapis.com/customsearch/v1?q=[SEARCH_TERM]&cx=005863448098847757980:hxez_lk5jy8&searchType=image&key=AIzaSyCZ0ok4aOSW31dz0TGa34Aegv-s-BrysCw&start=[PAGE_START]&fields=items%2Flink',
   stripePublishableKey: 'pk_test_0Opnf68xnyT0RVyCQjqw0ScB',
   googleAnalytics: {
